312eca4a8d89de58c1437c592f26fbe5b88c9b8c
meisterjustice <eziefulejustice@gmail.com>
2023-01-13 23:37:45 +0100

    chore: profile ui

---
+++ public/icon/sol.js
@@ -0,0 +1,61 @@
+const SolIcon = () => {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.94962 9.15882C2.02203 9.06643 2.12159 9.01253 2.22719 9.01253H11.8033C11.9783 9.01253 12.0658 9.28202 11.9421 9.43986L10.0504 11.8537C9.97797 11.9461 9.87841 12 9.77281 12H0.196716C0.0217275 12 -0.0657668 11.7305 0.057932 11.5727L1.94962 9.15882Z"
+        fill="url(#paint0_linear_49_673)"
+      />
+      <path
+        d="M1.94962 0.146295C2.02504 0.053898 2.12461 0 2.22719 0H11.8033C11.9783 0 12.0658 0.26949 11.9421 0.427334L10.0504 2.84119C9.97797 2.93359 9.87841 2.98749 9.77281 2.98749H0.196716C0.0217275 2.98749 -0.0657668 2.718 0.057932 2.56015L1.94962 0.146295Z"
+        fill="url(#paint1_linear_49_673)"
+      />
+      <path
+        d="M10.0504 4.62367C9.97797 4.53127 9.87841 4.47737 9.77281 4.47737H0.196716C0.0217275 4.47737 -0.0657668 4.74686 0.057932 4.90471L1.94962 7.31856C2.02203 7.41096 2.12159 7.46486 2.22719 7.46486H11.8033C11.9783 7.46486 12.0658 7.19537 11.9421 7.03753L10.0504 4.62367Z"
+        fill="url(#paint2_linear_49_673)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_49_673"
+          x1="-21.6436"
+          y1="-2.26693"
+          x2="-22.0584"
+          y2="14.0888"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#00FFA3" />
+          <stop offset="1" stop-color="#DC1FFF" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_49_673"
+          x1="-24.5415"
+          y1="-4.1975"
+          x2="-24.9563"
+          y2="12.1583"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#F8C6C6" />
+          <stop offset="1" stop-color="#FFD27B" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_49_673"
+          x1="-23.1018"
+          y1="-3.23838"
+          x2="-23.5166"
+          y2="13.1174"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#FFCAD3" />
+          <stop offset="1" stop-color="#FFCAD3" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default SolIcon;
