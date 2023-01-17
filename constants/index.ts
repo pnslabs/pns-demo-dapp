7e4823ebb15cc551850edd273b157179d505a61c
Njoku Emmanuel <kalunjoku123@gmail.com>
2023-01-17 08:54:13 +0300

    chore: update addresses and Abi

--- constants/index.ts
+++ constants/index.ts
@@ -1,7 +1,7 @@
 const registryAbi = require("./PNSRegistry.json");
 const resolverAbi = require("./PNSResolver.json");
 
-const registryAddress = "0x9d5a1f5Fde036f1b10E8Ff5BEC938D8b26a4c4bB";
-const resolverAddress = "0x6b1714EAC8968A01daA22f3Ff4BCf2FD03F66F66";
+const registryAddress = "0xD5469F3Ce6d6a097279F2ED9B1f1BE2789A3072b";
+const resolverAddress = "0xC256E2A99D7D973901718267f0C5A7E8585b776c";
 
 export { registryAddress, resolverAddress, registryAbi, resolverAbi };
